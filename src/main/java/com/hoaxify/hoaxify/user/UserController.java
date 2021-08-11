a5e4f9f29bfe50de4205ff927f6f9a64b18777d3
Gaurav Singh <grvsgh7@gmail.com>
2021-08-11 19:42:40 +0530

    added find by username functionality

--- src/main/java/com/hoaxify/hoaxify/user/UserController.java
+++ src/main/java/com/hoaxify/hoaxify/user/UserController.java
@@ -9,17 +9,16 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,6 +48,12 @@ public class UserController {
 		//return userService.getUsers().map(user->new UserVM(user));
 		//	OR
 		return userService.getUsers(loggedInUser, pageable).map(UserVM::new);
+	}
+	
+	@GetMapping("/users/{username}")
+	UserVM getUserByName(@PathVariable String username) {
+		User user = userService.getByUsername(username);
+		return new UserVM(user);
 	}
 	
 	@ExceptionHandler({MethodArgumentNotValidException.class})
