2376864239113ba607daa64e5a5965c170dd6c34
Gaurav Singh <grvsgh7@gmail.com>
2021-08-24 21:40:24 +0530

    added Hoax feed

--- src/main/java/com/hoaxify/hoaxify/hoax/HoaxController.java
+++ src/main/java/com/hoaxify/hoaxify/hoax/HoaxController.java
@@ -3,11 +3,16 @@ package com.hoaxify.hoaxify.hoax;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.hoaxify.hoax.vm.HoaxVM;
 import com.hoaxify.hoaxify.shared.CurrentUser;
 import com.hoaxify.hoaxify.user.User;
 
@@ -18,7 +23,17 @@ public class HoaxController {
 	private HoaxService service;
 	
 	@PostMapping("/hoaxes")
-	public void createHoax(@Valid @RequestBody Hoax hoax, @CurrentUser User user) {
-		service.save(user, hoax);
+	public HoaxVM createHoax(@Valid @RequestBody Hoax hoax, @CurrentUser User user) {
+		return new HoaxVM(service.save(user, hoax));
+	}
+	
+	@GetMapping("/hoaxes")
+	public Page<HoaxVM> getAllHoaxes(Pageable pageable) {
+		return service.getAllHoaxes(pageable).map(HoaxVM::new);
+	}
+	
+	@GetMapping("/users/{username}/hoaxes")
+	public Page<HoaxVM> getHoaxesOfUser(@PathVariable String username, Pageable pageable) {
+		return service.getHoaxesOfUser(username,pageable).map(HoaxVM::new);
 	}
 }
--- src/main/java/com/hoaxify/hoaxify/hoax/HoaxService.java
+++ src/main/java/com/hoaxify/hoaxify/hoax/HoaxService.java
@@ -2,22 +2,36 @@ package com.hoaxify.hoaxify.hoax;
 
 import java.util.Date;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserService;
 
 @Service
 public class HoaxService {
 	private HoaxRepository hoaxRepository;
+	private UserService userService;
 
-	public HoaxService(HoaxRepository hoaxRepository) {
+	public HoaxService(HoaxRepository hoaxRepository, UserService userService) {
 		super();
 		this.hoaxRepository = hoaxRepository;
+		this.userService = userService;
 	}
 	
-	public void save(User user, Hoax hoax) {
+	public Hoax save(User user, Hoax hoax) {
 		hoax.setTimestamp(new Date());
 		hoax.setUser(user);
-		hoaxRepository.save(hoax);
+		return hoaxRepository.save(hoax);
+	}
+
+	public Page<Hoax> getAllHoaxes(Pageable pageable) {
+		return hoaxRepository.findAll(pageable);
+	}
+
+	public Page<Hoax> getHoaxesOfUser(String username, Pageable pageable) {
+		User inDB = userService.getByUsername(username);
+		return hoaxRepository.findByUser(inDB, pageable);
 	}
 }
